7a38fbfc4913208a871f8fd0602320ebb5a9f36d
rahadityaputra <rahadit.a.p123@gmail.com>
2024-01-08 20:04:30 +0700

    mengatur pemilihan tema agar tidak berubah saat direfresh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const isiList = document.getElementById("input-list");
 const daftarList = document.querySelector(".box-list");
 const spanList = document.querySelectorAll(".box-list .list > span");
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 function tambahList() {
 	if (isiList.value != "" && cekAdaList() && daftarList.childElementCount < 5) {
@@ -17,7 +17,6 @@ function tambahList() {
 }
 
 function buatTombol(listBaru) {
-	
 	// membuat tombol selesai
 	const tombolSelesai = document.createElement("span");
 	tombolSelesai.classList.add("button", "button-finish");
@@ -25,7 +24,7 @@ function buatTombol(listBaru) {
 	<path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
 </svg>`;
 	listBaru.appendChild(tombolSelesai);
-	
+
 	//membuat tombol hapus
 	const tombolHapus = document.createElement("span");
 	tombolHapus.classList.add("button", "button-delete");
@@ -60,45 +59,55 @@ function turun(buttonFinish) {
 	daftarList.appendChild(list);
 }
 
-container.addEventListener('click', function (e) {
+container.addEventListener("click", function (e) {
 	// console.log(e.target);
-	if (e.target.className == 'button button-add' || e.target.parentNode.className == 'button button-add') {
+	if (e.target.className == "button button-add" || e.target.parentNode.className == "button button-add") {
 		tambahList();
-	} else if (e.target.className == 'button button-finish' || e.target.parentNode.className == 'button button-finish') {
-		if (e.target.className == 'button button-finish') {
-			e.target.previousElementSibling.style.textDecoration = 'line-through';
+	} else if (e.target.className == "button button-finish" || e.target.parentNode.className == "button button-finish") {
+		if (e.target.className == "button button-finish") {
+			e.target.previousElementSibling.style.textDecoration = "line-through";
 			turun(e.target);
-		} else if (e.target.parentNode.className == 'button button-finish') {
-			e.target.parentNode.previousElementSibling.style.textDecoration = 'line-through';
+		} else if (e.target.parentNode.className == "button button-finish") {
+			e.target.parentNode.previousElementSibling.style.textDecoration = "line-through";
 			turun(e.target.parentNode);
 		}
-	} else if (e.target.className == 'button button-delete' || e.target.parentNode.className == 'button button-delete') {
-		if (e.target.className == 'button button-delete') {
+	} else if (e.target.className == "button button-delete" || e.target.parentNode.className == "button button-delete") {
+		if (e.target.className == "button button-delete") {
 			e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-		} else if (e.target.parentNode.className == 'button button-delete') {
+		} else if (e.target.parentNode.className == "button button-delete") {
 			e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
-
 		}
-	} else if (e.target.className == 'button button-delete-all' || e.target.parentNode.className == 'button button-delete-all') {
-		daftarList.innerHTML = '';
+	} else if (e.target.className == "button button-delete-all" || e.target.parentNode.className == "button button-delete-all") {
+		daftarList.innerHTML = "";
 	}
-})
-
-
+});
 
 // untuk fitur mode gelap
-const buttonTheme = document.querySelector('.mode');
-const html = document.querySelector('html');
+const buttonTheme = document.querySelector(".mode");
+const html = document.querySelector("html");
 
-buttonTheme.addEventListener('click', function () {
-	html.classList.toggle('gelap');
-	if (html.className == 'gelap') {
+// local storage untuk mode tampilan light mode dan dark mode
+let modeSekarang = localStorage.getItem("modeSekarang");
+
+if (localStorage.getItem("modeSekarang") === "dark") {
+	html.classList.toggle("gelap");
+	buttonTheme.innerHTML = `<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"></path>
+	  </svg>`;
+}
+
+buttonTheme.addEventListener("click", function () {
+	html.classList.toggle("gelap");
+	if (html.className == "gelap") {
+		modeSekarang = localStorage.setItem("modeSekarang", "dark");
+
 		buttonTheme.innerHTML = `<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 		<path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"></path>
 	  </svg>`;
 	} else {
+		modeSekarang = localStorage.setItem("modeSekarang", "light");
 		buttonTheme.innerHTML = `<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 		<path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"></path>
 	</svg>`;
 	}
-})
+});
